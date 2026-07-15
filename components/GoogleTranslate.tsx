05344eb1a203bfe0494544145a5151bdb0b93e7f
Ssreejita <sreejitaswain07@gmail.com>
2026-07-15 10:36:37 +0530

    Add Google Translate support with reset to English option

--- components/GoogleTranslate.tsx
+++ components/GoogleTranslate.tsx
@@ -9,6 +9,12 @@ declare global {
   }
 }
 
+function resetToEnglish() {
+  document.cookie = 'googtrans=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC';
+  document.cookie = `googtrans=; path=/; domain=${window.location.hostname}; expires=Thu, 01 Jan 1970 00:00:00 UTC`;
+  window.location.reload();
+}
+
 export default function GoogleTranslate() {
   useEffect(() => {
     if (document.getElementById('google-translate-script')) return;
@@ -32,5 +38,15 @@ export default function GoogleTranslate() {
     document.body.appendChild(script);
   }, []);
 
-  return <div id="google_translate_element" className="translate-widget" />;
+  return (
+    <div className="space-y-1.5">
+      <div id="google_translate_element" className="translate-widget" />
+      <button
+        onClick={resetToEnglish}
+        className="text-[12px] font-semibold text-indigo-400 hover:text-indigo-300 transition-colors"
+      >
+        Reset to English
+      </button>
+    </div>
+  );
 }
